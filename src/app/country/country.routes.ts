550dcaa96bae0b9ee274356a9fb41c53d51f300e
eduardo355 <ingeniero.eduardo355@gmail.com>
2025-12-08 18:19:00 -0600

    feat: integrate Tailwind CSS and DaisyUI, add routing for country pages
    - Updated package.json to include Tailwind CSS and DaisyUI dependencies.
    - Removed placeholder content from app.html and added a router outlet.
    - Defined routes in app.routes.ts for HomePage and country-related pages.
    - Created country.routes.ts for country layout and by-capital page.
    - Implemented CountryLayout and ByCapitalPage components with basic templates.
    - Added HomePage component with a simple template.
    - Updated global styles to include Tailwind CSS and DaisyUI themes.

---
+++ src/app/country/country.routes.ts
@@ -0,0 +1,27 @@
+import { Routes } from '@angular/router';
+import { ByCapitalPage } from './pages/by-capital-page/by-capital-page';
+import { CountryLayout } from './layouts/CountryLayout/CountryLayout';
+
+const countryRoutes: Routes = [
+   {
+      path: '',
+      component: CountryLayout,
+      children: [
+         {
+            path: 'by-capital',
+            component: ByCapitalPage
+         },
+         {
+            path: '**',
+            redirectTo: 'by-capital'
+         }
+      ]
+   },
+   // {
+   //    path: 'country'
+   // },
+
+];
+
+
+export default countryRoutes
